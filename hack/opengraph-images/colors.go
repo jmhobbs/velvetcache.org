b3bb7ae001165f97089449d12be5631e545f1754
John Hobbs <john@velvetcache.org>
2024-03-04 13:53:25 -0600

    HTTP OG image generator

---
+++ hack/opengraph-images/colors.go
@@ -0,0 +1,26 @@
+package main
+
+import "strconv"
+
+var colors []string = []string{
+	"#e5b567",
+	"#b4d273",
+	"#e87d3e",
+	"#9e86c8",
+	"#b05279",
+	"#6c99bb",
+}
+
+// #xxxxxx -> (r, g, b)
+func hexColorToRGB(color string) (uint8, uint8, uint8, error) {
+	red, err := strconv.ParseUint(color[1:3], 16, 8)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	green, err := strconv.ParseUint(color[3:5], 16, 8)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	blue, err := strconv.ParseUint(color[5:7], 16, 8)
+	return uint8(red), uint8(green), uint8(blue), err
+}
